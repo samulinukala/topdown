ba436ade8cf9d1a27d30c87795a449d22604a296
samulinukala <71631113+samulinukala@users.noreply.github.com>
2022-02-10 14:57:41 +0200

    test

--- New Unity Project (4)/Assets/projectile.cs
+++ New Unity Project (4)/Assets/projectile.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class projectile : MonoBehaviour
 {
+    public float moveSpeed=50;
+    public Vector2 playerLoc;
+    public GameObject spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerLoc = spawnPoint.transform.position
+        GetComponent<Rigidbody2D>().AddForce(playerLoc.normalized*moveSpeed, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
