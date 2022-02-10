f313f4ad570fb4e45835d84a773276732a1ca88f
samulinukala <71631113+samulinukala@users.noreply.github.com>
2022-02-10 13:53:41 +0200

    made character sorta work

--- New Unity Project (4)/Assets/playermover.cs
+++ New Unity Project (4)/Assets/playermover.cs
@@ -9,10 +9,12 @@ public class playermover : MonoBehaviour
     public float moveForce=5;
     public Vector2 movementVector;
     public CharacterController controller;
-    public Vector3 aimVector;
+    public float deadZone=0.05f;
     public float contAHor;
     public float contAVer;
     public float angle;
+    public float damage = 33.3333f;
+    public float health = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +25,31 @@ public class playermover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
         movementVector = new Vector2(horizontal, vertical).normalized*moveForce*Time.deltaTime;
         contAHor = Input.GetAxis("AimHorizontalController");
         contAVer = Input.GetAxis("AimVerticalController");
         Debug.Log(contAHor+" , "+ contAVer);
-        //aimVector = new Vector3(contAHor, contAVer,0).normalized;
-        angle = Mathf.Atan2(contAVer, contAHor) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.EulerAngles(0, 0, angle);
-        Debug.Log("aim"+aimVector);
-        Debug.Log("move"+movementVector);
+
+
+        if (new Vector2(contAHor, contAVer).magnitude > deadZone)
+        {
+            angle = Mathf.Atan2(-contAVer, -contAHor);
+            transform.rotation = Quaternion.EulerAngles(0, 0, angle);
+
+        }
+       // Debug.Log("move"+movementVector);
         controller.Move(movementVector);
     }
+    public void takeDamage() 
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
