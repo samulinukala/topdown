4718a9a468f3783d217124a158cbe3fa8017c6b0
NiklasPirinen <bnipi003@edu.xamk.fi>
2022-02-10 14:32:37 +0200

    niklas commit
    player hurt

--- New Unity Project (4)/Assets/Scenes/Niklas/Scripts/playerHP.cs
+++ New Unity Project (4)/Assets/Scenes/Niklas/Scripts/playerHP.cs
@@ -4,15 +4,108 @@ using UnityEngine;
 
 public class playerHP : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float health;
+    public float maxHealth;
+
+    public GameObject fullHPSprite;
+    public GameObject hp3Sprite;
+    public GameObject hp2Sprite;
+    public GameObject hp1Sprite;
+
+    public bool invulnerable;
+    public float damageDelay;
+    public float deathDelay;
+    public GameObject hitParticle;
+
+    public Component[] spriteRenderers;
+
     void Start()
     {
-        
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        health = maxHealth;
+        fullHPSprite.SetActive(true);
+        hp3Sprite.SetActive(false);
+        hp2Sprite.SetActive(false);
+        hp1Sprite.SetActive(false);
+        invulnerable = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(health >= maxHealth)
+        {
+            health = maxHealth;
+            fullHPSprite.SetActive(true);
+            hp3Sprite.SetActive(false);
+            hp2Sprite.SetActive(false);
+            hp1Sprite.SetActive(false);
+        }
+        if (health == 3)
+        {
+            fullHPSprite.SetActive(false);
+            hp3Sprite.SetActive(true);
+            hp2Sprite.SetActive(false);
+            hp1Sprite.SetActive(false);
+        }
+        if (health == 2)
+        {
+            fullHPSprite.SetActive(false);
+            hp3Sprite.SetActive(false);
+            hp2Sprite.SetActive(true);
+            hp1Sprite.SetActive(false);
+        }
+        if (health == 1)
+        {
+            fullHPSprite.SetActive(false);
+            hp3Sprite.SetActive(false);
+            hp2Sprite.SetActive(false);
+            hp1Sprite.SetActive(true);
+        }
+        if (health <= 0)
+        {
+            health = 0;
+            StartCoroutine(deathDelayCo());
+        }
+        if (invulnerable)
+        {
+            foreach (SpriteRenderer obj in spriteRenderers)
+            {
+                obj.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
+        if (!invulnerable)
+        {
+            foreach (SpriteRenderer obj in spriteRenderers)
+            {
+                obj.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if(!invulnerable)
+        {
+            StartCoroutine(damageDelayCo());
+        }
+    }
+
+    public IEnumerator damageDelayCo()
+    {
+        invulnerable = true;
+        Instantiate(hitParticle, gameObject.transform.position, gameObject.transform.rotation);
+        health -= 1;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(damageDelay);
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        invulnerable = false;
+    }
+
+    public IEnumerator deathDelayCo()
+    {
+        Instantiate(hitParticle, gameObject.transform.position, gameObject.transform.rotation);
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(deathDelay);
+        Destroy(gameObject);
     }
 }
