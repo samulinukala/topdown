ff456714cf64ca4823cc7aac8445aaec1ace00be
samulinukala <71631113+samulinukala@users.noreply.github.com>
2022-02-24 14:57:55 +0200

    working ui func

---
+++ Assets/uiManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiManager : MonoBehaviour
+{
+    public PlayerUIControl[] PlayerUiElements;
+    public playermover[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i<players.Length-1;i++)
+        {
+            
+        }
+    }
+}
